78529b253f7e0c9d542d641c479b0739e9323ff7
ash <ashfixit@gmail.com>
2022-01-19 14:23:43 -0500

    try pdf-form-fill  .. for the same breakage

--- minifill/fll.js
+++ minifill/fll.js
@@ -1,15 +1,35 @@
 fs = require('fs');
 pdfFillForm=require('pdf-fill-form');
+pdfFormFill=require('pdf-form-fill');
 
 console.log("minifill"); 
 
 const pdf_template_path = 'retiree.pdf'; 
-const pdf_template_path2 = 'irsf1099r2022p4.pdf';
+//jjconst pdf_template_pathirs = 'irsf1099r2022p4.pdf';
+const pdf_template_pathirs = 'irsf1099r2022p4.pdf';
 var fields = { "Retiree Last Name" : 'fett' };
-var irsfields = {  'f2_1[0]':'stillfett' };
+//var irsfields = {  'topmostSubform_0_.Copy1_0_.LeftCol_ReadOrderControl_0_.f2_1_0_':'stillfett' }; // this is what lifecycleD thinks the full name is
+var irsfields = {  "f2_1[0]":'stillfett' }; // this is the form that pdftk seems to like
+//var irsfields = {  'payer_addr':'stillfett' }; // we can change the alternate field name in lifecycle designer
+//var irsfields = {  'topmostSubform[0].Page1[0].payer_addr[0]':'stillfett' }; // full path from pdftk dump_data_fields
 
 var outpdf = pdfFillForm.writeSync(pdf_template_path, fields, { "savetype":"imgpdf"}); // this works
 fs.writeFileSync( 'outret.pdf', outpdf); 
 
-var outpdf = pdfFillForm.writeSync(pdf_template_path2, irsfields, { "savetype":"imgpdf"}); // this doesn't work, pdf is not filled out
+var outpdf = pdfFillForm.writeSync(pdf_template_pathirs, irsfields, { "savetype":"imgpdf"}); // this doesn't work, pdf is not filled out
 fs.writeFileSync( 'outirs.pdf', outpdf); 
+
+console.log ("pdf-form-fill");
+
+
+const outpdfret2 = fs.createWriteStream("outret2.pdf"); // this works 
+pdfFormFill.fill(pdf_template_path, fields, { "savetype":"imgpdf", "verbose":true}).then ( 
+    stream => stream.pipe(outpdfret2)
+  ).catch(err => console.error(err)); 
+
+
+const outpdf2 = fs.createWriteStream("outisr2.pdf"); // this is broken, pdf not filled out
+pdfFormFill.fill(pdf_template_pathirs, irsfields, { "savetype":"imgpdf", "verbose":true}).then (
+    stream => stream.pipe(outpdf2)
+  ).catch(err => console.error(err)); 
+console.log ("done-form-fill");
