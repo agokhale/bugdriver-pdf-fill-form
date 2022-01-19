a51aca9a3ec8775497e9f358e88ea17a89ec44af
ash <ashfixit@gmail.com>
2022-01-19 09:49:39 -0500

    comment does/doesn't work

--- minifill/fll.js
+++ minifill/fll.js
@@ -8,8 +8,8 @@ const pdf_template_path2 = 'irsf1099r2022p4.pdf';
 var fields = { "Retiree Last Name" : 'fett' };
 var irsfields = {  'f2_1[0]':'stillfett' };
 
-var outpdf = pdfFillForm.writeSync(pdf_template_path, fields, { "savetype":"imgpdf"});
+var outpdf = pdfFillForm.writeSync(pdf_template_path, fields, { "savetype":"imgpdf"}); // this works
 fs.writeFileSync( 'outret.pdf', outpdf); 
 
-var outpdf = pdfFillForm.writeSync(pdf_template_path2, irsfields, { "savetype":"imgpdf"});
+var outpdf = pdfFillForm.writeSync(pdf_template_path2, irsfields, { "savetype":"imgpdf"}); // this doesn't work, pdf is not filled out
 fs.writeFileSync( 'outirs.pdf', outpdf); 
